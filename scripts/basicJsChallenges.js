5251bf4a1baa71b3c51f1f4baadacac7cef3964d
GreenCatInForest <event2016.2017@gmail.com>
2023-07-25 16:54:35 +0100

    link image to card

--- scripts/basicJsChallenges.js
+++ scripts/basicJsChallenges.js
@@ -1,11 +1,20 @@
+let basicJsChallengesContainer = document.querySelector(
+  "#basicJsChallengesContainer"
+);
+
 fetch("./data/basicJsChallenges.json")
   .then((response) => response.json())
-  .then((data) => {
-    console.log(data);
-    displaybasicJSChallenges(data);
+  .then((datas) => {
+    console.log(datas);
+    displaybasicJSChallenges(datas);
   })
   .catch((error) => console.error("Error fetching data:", error));
 
-let displaybasicJSChallenges = (data) => {
-  basicJsChallengesContainer;
+let displaybasicJSChallenges = (datas) => {
+  datas.forEach((data) => {
+    let basicJsChallengesCard = document.createElement("div");
+    basicJsChallengesCard.innerHTML = `
+        <img src="${data.image}"/>`;
+    basicJsChallengesContainer.appendChild(basicJsChallengesCard);
+  });
 };
