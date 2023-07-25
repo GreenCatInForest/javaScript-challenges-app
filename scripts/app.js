e76f6626569e4f800c55fbced8b54a8bb257e3f0
GreenCatInForest <event2016.2017@gmail.com>
2023-07-25 11:29:44 +0100

    add comment with breakdown calculation

--- scripts/app.js
+++ scripts/app.js
@@ -6,14 +6,38 @@ let resultFinonacci = document.querySelector("#resultFibonacci");
 
 let calculateFibonacci = () => {
   array = [0, 1];
-  let i1 = 0;
-  let i2 = 1;
+  let fi1 = 0;
+  let fi2 = 1;
   for (let i = 3; i <= numberForFibonacci.value; i++) {
-    iNextPlus = i1 + i2;
-    i1 = i2;
-    i2 = iNextPlus;
+    fiNextPlus = fi1 + fi2;
 
-    array.push(i2);
+    // 1 iteration fiNextPlus = 1
+    // 2 iteration fiNextPlus = 1+1=2
+    // 3 iteration fiNextPlus = 1+2=3
+    // 4 iteration fiNextPlus = 2+3=5
+    // 4 iteration fiNextPlus = 3+5=8
+
+    console.log(fiNextPlus);
+
+    fi1 = fi2;
+    // 1 iteration fi1 = 1
+    // 2 iteration fi1 = 1
+    // 3 iteration fi1 = 2
+    // 4 iteration fi1 = 3
+    // 5 iteration fi1 = 5
+
+    console.log(fi1);
+
+    fi2 = fiNextPlus;
+    // 1 iteration fi2 = 1
+    // 2 iteration fi2 = 2
+    // 3 iteration fi2 = 3
+    // 4 iteration fi2 = 5
+    // 5 iteration fi2 = 8
+
+    console.log(fi2);
+
+    array.push(fi2);
     console.log(array);
 
     // fibonacciSeries(8);
