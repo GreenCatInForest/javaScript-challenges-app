9d76c7c648f6df7d4984d7aaf4c4d1fb8643566c
GreenCatInForest <event2016.2017@gmail.com>
2023-07-26 22:08:49 +0100

    add event listener to startersChallenges, fixed script

---
+++ scripts/startersJsChallenges.js
@@ -0,0 +1,13 @@
+let navVariablesJsDataTypes = document.querySelector(
+  "#navVariablesJsDataTypes"
+);
+let navOperatorsJs = document.querySelector("#navOperatorsJs");
+let navConditionalsJs = document.querySelector("#navConditionalsJs");
+let navLoopsJs = document.querySelector("#navLoopsJs");
+let navFunctionsJs = document.querySelector("#navFunctionsJs");
+
+let showChallenge = () => {
+  console.log("success");
+};
+
+navVariablesJsDataTypes.addEventListener("click", showChallenge());
