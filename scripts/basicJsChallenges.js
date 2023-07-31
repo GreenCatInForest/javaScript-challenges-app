517844bb9c2749ccf2a74a49cb7bc749745f6289
GreenCatInForest <event2016.2017@gmail.com>
2023-07-31 21:08:50 +0100

    add functionality fetch basic challenges from main data.json

--- scripts/basicJsChallenges.js
+++ scripts/basicJsChallenges.js
@@ -2,29 +2,21 @@ let basicJsChallengesContainer = document.querySelector(
   "#basicJsChallengesContainer"
 );
 
-fetch("./data/basicJsChallenges.json")
+fetch("./data/data.json")
   .then((response) => response.json())
   .then((datas) => {
-    console.log(datas);
-    displaybasicJSChallenges(datas);
+    let listBasicJsChallenges = datas.challendes.basicJsChallenges;
+    listBasicJsChallenges.forEach((listBasicJsChallenge) => {
+      basicJsChallengesCard = document.createElement("div");
+      basicJsChallengesCard.setAttribute("id", "basicJsChallengesCard");
+      basicJsChallengesCard.innerHTML = `<h4>${listBasicJsChallenge.title}</h4>
+      <p>${listBasicJsChallenge.content}</p>
+      <img src="${listBasicJsChallenge.image}" alt="${listBasicJsChallenge.title}"/>
+      <div>
+      <a href="${listBasicJsChallenge.projectLink}">Explore</a>
+      <a href="${listBasicJsChallenge.projectGithubLink}">Github</a>
+      </div>`;
+      basicJsChallengesContainer.appendChild(basicJsChallengesCard);
+    });
   })
   .catch((error) => console.error("Error fetching data:", error));
-
-let displaybasicJSChallenges = (datas) => {
-  datas.forEach((data) => {
-    basicJsChallengesCard = document.createElement("div");
-    basicJsChallengesCard.setAttribute("id", "basicJsChallengesCard");
-
-    basicJsChallengesCard.innerHTML = `
-    <h4>${data.title}</h4>
-    <p>${data.content}</p>
-    <img src="${data.image}" alt="${data.title}"/>
-    <div>
-    <a href="${data.projectLink}">Explore</a>
-    <a href="${data.projectGithubLink}">Github</a>
-    </div>
-    
-    `;
-    basicJsChallengesContainer.appendChild(basicJsChallengesCard);
-  });
-};
