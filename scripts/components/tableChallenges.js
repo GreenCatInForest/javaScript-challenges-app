a3b7b0d642d7f597956345b20f099956134de75b
GreenCatInForest <event2016.2017@gmail.com>
2023-08-09 20:14:52 +0100

    improve table fetch

--- scripts/components/tableChallenges.js
+++ scripts/components/tableChallenges.js
@@ -2,9 +2,20 @@ const fetchChallengesTable = document.querySelector("#fetchAllChallengesTable");
 let table = document.createElement("table");
 fetchChallengesTable.appendChild(table);
 
-let createTable = () => {
+fetch("./data/data.json")
+  .then((response) => response.json())
+  .then((datas) => {
+    console.log(datas);
+    let challengesDatas = datas.challenges;
+    sortDataChallenges(challengesDatas);
+  })
+  .catch((error) => console.error(error));
+
+//
+const createTable = () => {
   let tableHead = document.createElement("thead");
   let tableTh = document.createElement("th");
+  tableTh.textContent = innerHtmlTh;
 
   let tableBody = document.createElement("tbody");
   let tableBodyRow = document.createElement("tr");
@@ -18,29 +29,61 @@ let createTable = () => {
   table.appendChild(tableBody);
 };
 
-const fetchDataChallenges = () => {
-  fetch("./data/data.json")
-    .then((response) => response.json())
-    .then((datas) => {
-      console.log(datas);
-      let challengesDatas = datas.challenges;
-      sortDataChallenges(challengesDatas);
-    })
-    .catch((error) => console.error(error));
-};
-
 const sortDataChallenges = (challengesDatas) => {
   challengesDatas.forEach((challengesData) => {
     if (challengesData.name === "basicJsChallenges") {
-      console.log("+");
+      let basicChallengesData = challengesData.allChallenges;
+      console.log(basicChallengesData);
+
+      basicChallengesData.forEach((basicChallengeData) => {
+        console.log(basicChallengeData);
+        innerHtmlTh = basicChallengeData.title;
+        createTable();
+      });
+    } else {
+      console.log(challengesData);
+      innerHtmlTh = challengesData.title;
+      createTable();
     }
-    console.log(challengesData.name);
   });
 };
 
-const displayChallengesTable = () => {
-  fetchDataChallenges();
-  createTable();
-};
+// const displayChallengesTable = () => {
+//   createTable();
+// };
 
-displayChallengesTable();
+// displayChallengesTable();
+
+// let addButtons = (mainChallengesArrayDatas) => {
+//       let tableRowButton = document.createElement("tr");
+//       let tableCellButton = document.createElement("td");
+
+//       let tableCheatSheetButton = document.createElement("button");
+//       let tableToChallengeButton = document.createElement("button");
+//       let tableAddChallengeButton = document.createElement("button");
+
+//       tableRowButton.className = "tableRowButton";
+//       tableCellButton.className = "tableCellButton";
+//       tableCheatSheetButton.className = "tableCheatSheetButton";
+//       tableToChallengeButton.className = "tableToChallengeButton";
+//       tableAddChallengeButton.className = "tableAddChallengeButton";
+
+//       tableCheatSheetButton.innerHTML = "Cheatsheet";
+//       tableToChallengeButton.innerHTML = "See the solution";
+//       tableAddChallengeButton.innerHTML = "Propose your challenge";
+
+//       if (mainChallengesArrayDatas[0].name === "basicJsChallenges") {
+//         tableCellButton.appendChild(tableCheatSheetButton);
+//         tableCellButton.appendChild(tableToChallengeButton);
+//         tableCellButton.appendChild(tableAddChallengeButton);
+//         tableRowButton.appendChild(tableCellButton);
+//         tbody.appendChild(tableRowButton);
+//       }
+
+//     tableHeadCellButton.appendChild(tableHeadCheatsheetButton);
+//     tableHeadCellButton.appendChild(tableHeadToChallengeButton);
+//     tableHeadCellButton.appendChild(tableAddChallengeButton);
+//     tableHeadRowButton.appendChild(tableHeadCellButton);
+
+//     table.appendChild(tableHeadRowButton);
+//     };
